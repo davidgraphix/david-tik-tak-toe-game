2a4fefdfce184febb499cc675bac2e3f04165a64
David smart <bamideledavidsmart40@gmail.com>
2024-04-06 12:24:47 +0100

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,2 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Board from './Board';
+
+function Game({ playerOne, playWithComputer, onMainMenu }) {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+    const winner = calculateWinner(squares);
+
+    const handleComputerMove = () => {
+        // Prevent computer moves if the game is won or all squares are filled
+        if (winner || squares.every((square) => square !== null)) {
+          return;
+        }
