a1407ddd92b7861483925552dde1e362a3fc78bd
David smart <bamideledavidsmart40@gmail.com>
2024-04-06 15:21:41 +0100

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,36 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import Board from './Board';
+import calculateWinner from './CalculateWinner';
 
 function Game({ playerOne, playWithComputer, onMainMenu }) {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
-    const winner = calculateWinner(squares);
-
-    const handleComputerMove = () => {
-        if (winner || squares.every((square) => square !== null)) {
-          return;
-        }
-
-        const availableSquares = squares
-      .map((sq, idx) => (sq === null ? idx : null))
-      .filter((val) => val !== null);
-    const randomSquare =
-      availableSquares[Math.floor(Math.random() * availableSquares.length)];
-    if (randomSquare !== undefined) {
-      setTimeout(() => {
-        squares[randomSquare] = 'O';
-        setSquares(squares.slice());
-        setXIsNext(true);
-      }, 500);
-    }
-  };
-
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(squares);
 
   useEffect(() => {
+    const handleComputerMove = () => {
+      // Prevent computer moves if the game is won or all squares are filled
+      if (winner || squares.every((square) => square !== null)) {
+        return;
+      }
+
+      const availableSquares = squares
+        .map((sq, idx) => (sq === null ? idx : null))
+        .filter((val) => val !== null);
+      const randomSquare =
+        availableSquares[Math.floor(Math.random() * availableSquares.length)];
+      if (randomSquare !== undefined) {
+        setTimeout(() => {
+          squares[randomSquare] = 'O';
+          setSquares(squares.slice());
+          setXIsNext(true);
+        }, 500);
+      }
+    };
+
+  
     if (playWithComputer && !xIsNext) {
       handleComputerMove();
     }
-  }, [xIsNext, playWithComputer, squares]);
+  }, [xIsNext, playWithComputer, squares, winner]);
 
   const handleClick = (i) => {
     if (calculateWinner(squares) || squares[i]) {
