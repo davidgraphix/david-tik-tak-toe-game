9ef15ccef98676596873386c90ace7dcc490721a
David smart <bamideledavidsmart40@gmail.com>
2024-04-06 12:15:34 +0100

    Create and Update Setup.js

---
+++ src/components/Setup.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+
+function SetUp({ onStart }){
+    const [mode, setMode] = useState(null);
+    const [playerOne, setPlayerOne] = useState('');
+
+    const handleStart = (isSInglePlayer) =>{
+        onStart(playerOne, isSInglePlayer);
+    };
+
+    return(
+        <div className="setup">
+            {mode === null && (
+                <div>
+                    <button onClick={() => setMode('single')}>1 Player</button>
+                    <button onClick={() => setMode('multi')}>2 Player</button>
+                </div>
+            )}
+
+            {mode && (
+                <div>
+                    <input autoFocus placeholder="Player One Name" value={playerOne} onChange={(e) => setPlayerOne(e.target.value)}/>
+                    <button onClick={() => handleStart(mode === 'single')}>Start Game</button>
+                    {mode === 'multi' && <button onClick={() => setMode(null)}>Back</button>}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default SetUp;
